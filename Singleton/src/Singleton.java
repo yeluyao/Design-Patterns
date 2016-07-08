8e8329406c5bdda102d32758e672f2f12fc12d18
yeluyao <327408759@qq.com>
2016-07-08 19:10:25 +0800

    一个比较完善的单例模式

--- Singleton/src/Singleton.java
+++ Singleton/src/Singleton.java
@@ -11,29 +11,15 @@
 */
 
 public class Singleton {
-	//持有私有静态实例，防止被引用，此处赋值为null，目的是实现延迟加载
-	private static Singleton instance=new Singleton();
+	
 	//私有构造方法，防止被实例化
 	private Singleton(){
 	}
-	//静态工程方法，创建实例
-	//为了能用在多线程环境下，对getInstance添加synchronized关键字
-/**
-	用synchronized关键字锁住对象，会使得性能有所下降，因为每次调用getInstance()都要对对象上锁，事实上，只有在第一次
-	创建对象的时候需要加锁，之后就不需要了，所以需要改进
-*/
-	/**
-	在Java指令中创建对象和赋值操作是分开进行的，也就是说instance=new Singleton();语句是分两步进行的，但是JVM并不保证
-	这两个操作的先后顺序，也就是说有可能JVM会为新的Singleton实例分配空间，然后直接赋值给instance成员，然后再去初始化这个Singleton实例。
-	例如现在有A、B两个线程：
-a>A、B线程同时进入了第一个if判断
-b>A首先进入synchronized块，由于instance为null，所以它执行instance = new Singleton();
-c>由于JVM内部的优化机制，JVM先画出了一些分配给Singleton实例的空白内存，并赋值给instance成员（注意此时JVM没有开始初始化这个实例），然后A离开了synchronized块。
-d>B进入synchronized块，由于instance此时不是null，因此它马上离开了synchronized块并将结果返回给调用该方法的程序。
-e>此时B线程打算使用Singleton实例，却发现它没有被初始化，于是错误发生了。
-所以程序还是有可能发生错误，其实程序在运行过程是很复杂的，从这点我们就可以看出，尤其是在写多线程环境下的程序更有难度，有挑战性。我们对该程序做进一步优化：
-*/
-
+	//使用内部类来维护单例
+	private static class SingletonFactory{
+		private static Singleton instance=new Singleton();
+	}
+	//获取实例
 	public static  Singleton getInstance(){		
 		return SingletonFactory.instance;
 	}
